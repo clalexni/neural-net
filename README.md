348218ea5883d2863c1885e6cdd6c04f56bb6cf4
clalexni <cl.alexni@gmail.com>
2020-10-30 14:38:32 -0500

    parse data

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # Neural Net
-Linear Classification with Sigmoid Regression
+- Linear Classification with Sigmoid Regression
+
+## Goal:
+
+## Requirements:
 
 ## Notes:
-### Gradient Descent with logistic regression
+### Description:
+- fake attribute is explicitly provided in dataset
+- take arguments: training_file test_file learning_rate iteration
+
+### Update RUle:
+- Gradient Descent with logistic regression:
 w_i = w_i + alpha * (y_k - sigmoid(wx^T)) * x_i * sigmoid'(wx^T)
--- neural_net.py
+++ neural_net.py
@@ -1,3 +1,39 @@
 #!/usr/bin/env python3.6
+import sys
+from itertools import islice
 
-print('test')
+class DataSet:
+  """
+  dataset fields:
+  d.ts          An example matrix (training set). It is basically a list of examples, 
+                each example is a list of attribute values + class value
+  d.col_names   A list of name (label) corresponding to the matrix's column
+  """
+  def __init__(self, col_names=None, ts=None):
+    self.col_names = col_names
+    self.ts = ts
+
+def parse_data(f):
+  """
+  parse data from file
+  return a list of column names and an exmaple matrix
+  """
+  with open(f, 'r') as file:
+    data = file.readlines()
+    col_names = data[0].split()
+    examples = [line.split() for line in islice(data, 1, len(data))]
+    return col_names, examples
+
+
+if __name__ == '__main__':
+  train = sys.argv[1]
+  test = sys.argv[2]
+  alpha = sys.argv[3]
+  iteration = sys.argv[4]
+
+  train_ds = DataSet(*parse_data(train))
+  #print(train_ds.col_names)
+
+
+
+
